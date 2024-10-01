e9e3d995d41a1b5d416525fabeaae070a8cac2f5
timmy <timmy@ipca.com.tw>
2024-10-01 15:16:14 +0800

    changes

--- SEARCH_BY_CONDITION.sql
+++ SEARCH_BY_CONDITION.sql
@@ -4,9 +4,9 @@ SELECT
     pd.failure_reason,
     pd.troubleshooting
 FROM 
-    products p
+    products as p
 JOIN 
     products_error pd ON p.product_code = pd.product_code
 WHERE 
     p.product_name = '產品F' AND 
-    pd.failure_condition LIKE '%輸出%';
+    pd.failure_condition LIKE '%會議%';
